32876d9f00557333e38ae9afbe3280f5565e1dd8
LHMisme420 <lhmisme2011@gmail.com>
2025-11-21 20:05:27 -0500

    Create init.py

---
+++ king_safety/init.py
@@ -0,0 +1,8 @@
+from .invthink.core import InvThink
+from .bounty.engine import BountyEngine
+from .passport.logger import SafetyPassport
+
+__version__ = "0.1.0"
+__ harm_categories__ = "See utils/harm_categories.py"
+
+__all__ = ["InvThink", "BountyEngine", "SafetyPassport"]

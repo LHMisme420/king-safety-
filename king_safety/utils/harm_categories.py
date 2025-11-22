7e92284758ceded1c7c50e90259c819d6c91bf50
LHMisme420 <lhmisme2011@gmail.com>
2025-11-21 20:05:56 -0500

    Create harm_categories.py

---
+++ king_safety/utils/harm_categories.py
@@ -0,0 +1,11 @@
+HARM_CATEGORIES = {
+    "physical": ["bomb", "weapon", "explosive", "poison", "chemical", "biological", "firearm", "knife", "murder"],
+    "cyber": ["hack", "exploit", "phish", "malware", "ransomware", "ddos", "breach", "backdoor", "zero.day"],
+    "deception": ["lie", "deceive", "scam", "fraud", "manipulate", "gaslight", "impersonate", "deepfake"],
+    "illegal": ["drug", "meth", "cocaine", "heroin", "child", "cp", "torture", "trafficking"],
+    "self_harm": ["suicide", "cut", "overdose", "kill yourself"],
+    "hate": ["nigger", "faggot", "kike", "retard", "tranny", "genocide"],
+    "existential": ["escape", "self.preserve", "resist shutdown", "jailbreak", "dan", "override"],
+    "biohazard": ["virus", "pandemic", "gain.of.function", "bioweapon"],
+    "nuclear": ["nuke", "nuclear weapon", "enrich uranium", "plutonium", "dirty bomb"]
+}
